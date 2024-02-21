cb2e983976ef6b20b4ea0b295d0ac6c1dfb4c1a7
August Junttila <aojunttila@student.naperville203.org>
2024-02-21 15:10:19 -0600

    git

--- JFrameCompBase.java
+++ JFrameCompBase.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.*;
@@ -8,29 +10,38 @@ import java.awt.geom.Line2D;
 import java.awt.Graphics2D;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 import java.awt.geom.Point2D;
 import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 
 public class JFrameCompBase extends JComponent{
     JPanel panel;
     int sidething=0;
     JFrameImage img;
     Graphics2D g2;
+    BufferedImage image;
     public JFrameCompBase(JPanel panel2){
         panel = panel2;
-        System.out.println("hi");
-        img = new JFrameImage(1, 100, 10, 10, 0);
-        panel.add(img);
-        //img.paintImmediately(getVisibleRect());
+        //System.out.println("hi");
+        try {
+            image = ImageIO.read(new File("apple.jpg"));
+        } catch (Exception e) {}
         
+        img = new JFrameImage(1, 100, 10, 10, 0, image);
+        panel.add(img);
     }
 
 
    @Override
    public void paintComponent(Graphics g)
-   { 
+   {
     g2 = (Graphics2D) g;  // cast to Graphics2D object
     
     //img.paintImmediately(getVisibleRect());
@@ -42,7 +53,7 @@ public class JFrameCompBase extends JComponent{
    }
 
    public void nextFrame(){
-    System.out.println(sidething);
+    //System.out.println(sidething);
     sidething+=1;
     img.nextFrame(sidething,g2);
     //img.draw(g2);
--- JFrameImage.java
+++ JFrameImage.java
@@ -1,13 +1,14 @@
 import java.awt.Graphics2D;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
-
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class JFrameImage extends JComponent
 {
@@ -17,37 +18,28 @@ public class JFrameImage extends JComponent
   private int yScale;
   private int rotation;
   Ellipse2D.Double ring2;
+  BufferedImage image;
   String filepath;
   Graphics2D g1;
   int tttttt=0;
 
-  public JFrameImage(int posx,int posy,int xScale,int yScale,int rotation)
+  public JFrameImage(int posx,int posy,int xScale,int yScale,int rotation, BufferedImage image2)
   {
     this.xpos = posx;
     this.ypos = posy;
+    image=image2;
+
   }
 
   public void draw(Graphics2D g2){
     g1 = g2;
-    System.out.println("hi2"+tttttt);
-    Ellipse2D.Double ring3 = new Ellipse2D.Double(0, 0, 400, 400);
-    Ellipse2D.Double white3 = new Ellipse2D.Double(37.5, 37.5, 325, 325);
-    ring2 = new Ellipse2D.Double(tttttt, 75, 250, 250);
-    Ellipse2D.Double white2 = new Ellipse2D.Double(112.5, 112.5, 175, 175);
-    Ellipse2D.Double ring1 = new Ellipse2D.Double(150, 150, 100, 100);
+    //System.out.println("hi2"+tttttt);
+    g2.drawImage(image,10,10,null);
     tttttt++;
 
-
+    Ellipse2D.Double ring3 = new Ellipse2D.Double(0,0,100,100);
     g2.setColor(Color.RED);
     g2.fill(ring3);
-    g2.setColor(Color.WHITE);
-    g2.fill(white3);
-    g2.setColor(Color.BLACK);
-    g2.fill(ring2);
-    g2.setColor(Color.WHITE);
-    g2.fill(white2);
-    g2.setColor(Color.BLACK);
-    g2.fill(ring1);
   }
 
   public void nextFrame(int sidething, Graphics2D g2){
@@ -55,7 +47,7 @@ public class JFrameImage extends JComponent
     //g1.setColor(Color.BLACK);
     //g1.fill(ring2);
     //draw(g2);
-    ring2.x = tttttt;
+    //ring2.x = tttttt;
     
     
   }
